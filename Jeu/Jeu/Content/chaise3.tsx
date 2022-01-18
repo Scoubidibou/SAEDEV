eae963d096c3568ba7ad755488213abbcf68d74e
Matis Wostry <Matis.Wostry@etu.univ-savoie.fr>
2022-01-18 18:30:53 +0100

    Toutes les maps sont la

---
+++ Jeu/Jeu/Content/chaise3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="chaise3" tilewidth="16" tileheight="16" tilecount="1" columns="1">
+ <image source="chaise3.gif" width="17" height="16"/>
+</tileset>
---
+++ Jeu/Jeu/Content/tiles_cus_irongate.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="tiles_cus_irongate" tilewidth="16" tileheight="16" tilecount="100" columns="4">
+ <image source="tiles_cus_irongate.gif" width="64" height="400"/>
+</tileset>
