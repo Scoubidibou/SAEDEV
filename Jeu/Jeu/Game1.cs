41a05bbd40a2542713bba0d4448fba44b50e5617
Tessa Chamoley <tessa.chamoley@etu.univ-smb.fr>
2022-01-19 10:01:53 +0100

    nik

--- Jeu/Jeu/Game1.cs
+++ Jeu/Jeu/Game1.cs
@@ -45,7 +45,7 @@ namespace Jeu
         private TypeAnimation _animation;
         private float _chrono;
 
-        //???
+        //random prof
         private Random tete = new Random();
         private int temps;
 
